3a90154682440708169a4735445f69258f0c5709
Emily <emilyalmeeidas@gmail.com>
2021-08-12 20:43:47 -0300

    Não aceitando qualquer caractere nos operadores

--- calculator.cpp
+++ calculator.cpp
@@ -1,27 +1,32 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
     int  valorA, valorB;
-    char operador;
+    string operador;
 
     cout << "Digite um número" << endl;
     cin >> valorA;
     cout << "Digite o operador (+, -, *, /)" << endl;
     cin >> operador;
+    while(operador != "+" && operador != "-" && operador != "*" && operador != "/" ){
+        cout << "Operador não existente, digite novamente" << endl;
+        cin >> operador;
+    }
     cout << "Digite mais um valor" << endl;
     cin >> valorB;
 
-    if(operador == '+'){
+    if(operador == "+"){
         cout << "= " << valorA + valorB << endl;
     }
-    else if(operador == '-'){
+    else if(operador == "-"){
         cout << "= " << valorA - valorB << endl;
     }
-    else if(operador == '*'){
+    else if(operador == "*"){
         cout << "= " << valorA + valorB << endl;
     }
-    else if(operador == '/'){
+    else if(operador == "/"){
         cout << "= " << valorA / valorB << endl;
     }
 
